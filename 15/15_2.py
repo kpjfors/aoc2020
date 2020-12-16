cf774c78e93a69d8e71391ec3fa4e92b1124c4e0
kpjfors <johanfors93@gmail.com>
2020-12-16 23:12:13 +0100

    Day 15.

---
+++ 15/15_2.py
@@ -0,0 +1,29 @@
+def main(inp):
+    inp = list(map(int,inp.split(",")))
+    nums = {}
+    i = 0
+    round = 1
+    latest = 0
+    said = 0
+    for i in range(len(inp)):
+        nums[inp[i]] = i+1
+        latest = nums[inp[i]]
+        round += 1
+
+    while True:
+        if not said in nums.keys():
+            nextsaid = 0
+        else:
+            nextsaid = round - nums[said]
+        nums[said] = round
+        said = nextsaid
+        round += 1
+        if round % 1000000 == 0: #SLOW, approx 1 min.
+            print(round)
+        if round == 30000000:
+            return said
+
+if __name__ == "__main__":
+    f = open("inp15_1.txt", "r")
+    inp = f.read()
+    print(main(inp))
